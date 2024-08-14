7ad0842df18f61df3bf18af226ed3095fc6da624
JuhainaSubhi <subhi.juhaina98@gmail.com>
2024-08-14 12:09:49 +0400

    Added /greet/<name> route for personalized greetings

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/greet/<name>')
+def greet(name):
+    return f'Hello, {name}!'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
